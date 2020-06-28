2fda07e9fbe54e9df0f81b8c8d35fa9581f6d15d
Thomas Price <thomas.price@acresoftware.com>
2020-06-28 18:06:32 +0100

    Add

--- origo-occupation-list.js
+++ origo-occupation-list.js
@@ -4,7 +4,7 @@ import data from "./data.json";
 
 const occupationMap = {};
 
-const mapData = (acc, item) => {
+export const mapData = (acc, item) => {
   occupationMap[data[item].toLowerCase()] = item;
   (acc.codes || []).push(item);
   (acc.occupations || []).push(data[item]);
@@ -28,19 +28,4 @@ export const getOccupations = () => occupations;
 
 export const getOccupationList = () => Object.values(data);
 
-console.log(
-  "occupationMap",
-  getCode("Power Station Manager"),
-  getOccupation("AAB00073")
-);
-
-// export const overwrite = (sicArray) => {
-//   if (!sicArray || !sicArray.length) return;
-//   sicArray.forEach((sic) => {
-//     const foundIndex = data.findIndex((item) => item.code === String(sic.code));
-//     if (foundIndex > -1) {
-//       data[foundIndex] = sic;
-//       mapCodeAndDescription(sic);
-//     }
-//   });
-// };
+export const getData = () => data;
--- test/origo-occupation-list.spec.js
+++ test/origo-occupation-list.spec.js
@@ -1,11 +1,12 @@
 import {
+  mapData,
   getCode,
   getCodes,
-  getCodeList,
-  getDescription,
-  getDescriptions,
-  getDescriptionList,
-  overwrite,
+  getCodesList,
+  getOccupation,
+  getOccupations,
+  getOccupationList,
+  getData,
 } from "../sic-list";
 
 import data from "../data.json";
@@ -15,69 +16,5 @@ describe("getCode", () => {
     const result = "43390";
     const description = "Other building completion and finishing";
     expect(getCode(description)).toBe(result);
-  });
-
-  test("returns undefined for invalid description", () => {
-    const result = undefined;
-    // invalid description
-    const description = "01101000 01100101 01101100 01101100 01101111";
-    expect(getCode(description)).toBe(result);
-  });
-});
-
-describe("getCodes", () => {
-  test("returns codes", () => {
-    const result = data.map((item) => item.code);
-    expect(JSON.stringify(getCodes())).toBe(JSON.stringify(result));
-  });
-});
-
-describe("getCodeList", () => {
-  test("returns code to descriptions map", () => {
-    const result = data.reduce(
-      (acc, datum) => ({ ...acc, ...{ [datum.code]: datum.description } }),
-      {}
-    );
-    expect(JSON.stringify(getCodeList())).toBe(JSON.stringify(result));
-  });
-});
-
-describe("getDescription", () => {
-  test("returns description for given code of type string", () => {
-    const code = "20160";
-    const result = "Manufacture of plastics in primary forms";
-    expect(getDescription(code)).toBe(result);
-  });
-});
-
-describe("getDescriptions", () => {
-  test("returns descriptions", () => {
-    const result = data.map((item) => item.description);
-    expect(JSON.stringify(getDescriptions())).toBe(JSON.stringify(result));
-  });
-});
-
-describe("getDescriptionList", () => {
-  test("returns description for given code", () => {
-    const result = data.reduce(
-      (acc, datum) => ({
-        ...acc,
-        ...{ [datum.description.toLowerCase()]: datum.code },
-      }),
-      {}
-    );
-    expect(JSON.stringify(getDescriptionList())).toBe(JSON.stringify(result));
-  });
-});
-
-describe("overwrite", () => {
-  test("overwrite description for given codes of type string", () => {
-    const code = "01210";
-    const oldDescription = "Growing of grapes";
-    const newDescription = "Growing of white, red and black grapes";
-    const correctedData = [{ code: code, description: newDescription }];
-    expect(getDescription(code)).toBe(oldDescription);
-    overwrite(correctedData);
-    expect(getDescription(code)).toBe(newDescription);
   });
 });
